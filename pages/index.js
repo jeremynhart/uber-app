f5ed376325b8c9dc83ea45eeb03c6b71b592ca30
Jeremy Hart <jeremynhart@gmail.com>
2022-03-21 22:34:11 -0500

    Navbar Design Completed

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Navbar from "../components/Navbar"
+
 const style = {
   wrapper: `h-screen w-screen flex flex-col`,
 }
@@ -5,6 +7,7 @@ const style = {
 export default function Home() {
   return (
     <div className={style.wrapper}>
+      <Navbar/>
       <div className={style.main}>
       </div>
       <div className={style.rideRequestContainer}>
